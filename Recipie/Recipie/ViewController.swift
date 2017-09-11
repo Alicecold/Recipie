dc76cc07240e418ee39ac7d6431654ce7728fb55
Alice Darner <alicecoldangel@gmail.com>
2017-09-11 09:36:52 +0200

    Basics :tada:

---
+++ Recipie/Recipie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipie
+//
+//  Created by Alice Darner on 2017-09-11.
+//  Copyright © 2017 Alice Darner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
